67c2f9de6314301aa5d62e3f7335f34348cb5c51
borisovMIROSLAV <miroslav.b.borisov@abv.bg>
2023-01-23 16:42:23 +0200

    changes

--- guessAnumber.js
+++ guessAnumber.js
@@ -1,14 +1,14 @@
 function guessAnumber() {
     const readline = require('readline').createInterface({
         input: process.stdin,
-        output: process.stdout,
+        output: process.stdout
     });
 
     let computerGuess = Math.floor(Math.random() * 100);
     let guess;
 
     let recursiveAsyncReadLine = function () {
-        readline.question('Guess the number(0-100): ', number => {
+        readline.question('Guess the number(0-100): ', (number) => {
             guess = Number(number);
 
             if (guess <= 100 && guess >= 0) {
