0b72f1ce729370200609dab55248e8c7b42d03fa
borisovMIROSLAV <miroslav.b.borisov@abv.bg>
2023-01-23 15:10:17 +0200

    created readline

--- guessAnumber.js
+++ guessAnumber.js
@@ -1,1 +1,20 @@
-const readline =  
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+rl.on('line', (line) => {
+    console.log(`Received: ${line}`);
+});
+
+let computerGuess = Math.floor(Math.random() * 100);
+
+let recursiveAsyncReadLine = function () {
+    rl.question('Guess the number(0-100): ', number => {
+
+    });
+}
+recursiveAsyncReadLine();
+
+// console.log(computerGuess)
