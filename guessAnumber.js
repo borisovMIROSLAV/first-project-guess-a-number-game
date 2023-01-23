795d50ba5d5387c2b46223b28f29ecb57a749a95
borisovMIROSLAV <miroslav.b.borisov@abv.bg>
2023-01-23 14:34:10 +0200

    Update guessAnumber.js

--- guessAnumber.js
+++ guessAnumber.js
@@ -1,6 +1,32 @@
-function solve() {
+// const readline = require('readline').createInterface({
+//     input: process.stdit,
+//     output: process.stdout
+// });
 
+function guessNumber() {
+
+    const computerGuess = Math.floor(Math.random() * 100);
+    let number = parseInt(prompt('Guess the number (0-100): '));
+
+    while (number != computerGuess) {
+        number = parseInt(prompt('Guess the number (0-100): '));
+    }
+
+    if (number === computerGuess) {
+        console.log('You guessed the correct number');
+    }
+    else {
+        console.log('Try again')
+    }
     
-
 }
-solve();
+guessNumber();
+
+// let computerGuess = Math.floor(Math.random() * 100);
+
+// let recursiveAsyncReadLine = function () {
+//     readline.question('Guess the number (0-100): ', (number) => {
+
+//     });
+// }
+// recursiveAsyncReadLine();
